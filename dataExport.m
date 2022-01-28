3a976e9278485eba84b1ab881caa5408cbd332fe
Erick <ahmederick00@gmail.com>
2022-01-29 00:10:03 +0100

    remove temp file

--- dataExport.m
+++ dataExport.m
@@ -4,12 +4,12 @@ clc;
 prompt = 'Press "1" for front or "2" for rear: ';
 side = input(prompt, 's');
 
-selectSide(side);
+[content, fileName, sheetName] = selectSide(side);
 
 fprintf('Initializing exporting process \n');
 
 for i=1:length(content)
-    writetable(content, filename, 'Sheet', sheetName, 'WriteVariableNames', true);
+    writetable(content, fileName, 'Sheet', sheetName, 'WriteVariableNames', true);
     end;
-    
+
 fprintf('Finished exporting')
--- selectSide.m
+++ selectSide.m
@@ -1,14 +1,14 @@
-function [content, filename, sheetName] = selectSide(input);
+function [content, fileName, sheetName] = selectSide(input);
 
 %input 1 is front, 2 is rear
 
 if rem(input, 2) == 0
     content = who('-file', 'M22H_FrontSuspension_Data.mat');
-    filename = 'FrontSuspension_Exported.xlsx';
+    fileName = 'FrontSuspension_Exported.xlsx';
     sheetName = 'Front';
 elseif rem(input, 2) ~= 0
     content = who('-file', 'M22H_RearSuspension_Data.mat');
-    filename = 'RearSuspension_Exported.xlsx';
+    fileName = 'RearSuspension_Exported.xlsx';
     sheetName = 'Rear';
 else
     fprintf = 'An error occured!';
